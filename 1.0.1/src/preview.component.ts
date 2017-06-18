fc052c7eef7f77cf27a119f2e3fbc7c8563ae693
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-06-18 22:12:05 +0200

    1.0.1 preview

---
+++ 1.0.1/src/preview.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'preview',
+  template: `
+  <div
+    class="preview"
+    [formGroup]="form"
+  >
+    <h3>Toggle</h3>
+
+    Name of the cat: <br>
+    <input
+      type="text"
+      formControlName="cat"
+      [ngStyle]="styles.base"
+    />
+    <br><br>
+
+    Is the cat dead? <br><br>
+    <cloukit-toggle
+      [form]="form"
+      controlName="isCatDead"
+    ></cloukit-toggle>
+
+    <br><br><hr><br><br>
+
+    Reactive Form Result:
+    <pre>{{getFormResult()}}</pre>
+
+  </div>`,
+  styles: [ '.preview { font-family:sans-serif' ],
+})
+export class PreviewComponent {
+  styles = { };
+  form: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.form = fb.group({
+      cat: 'Schrödingers',
+      isCatDead: false
+    });
+
+    this.styles.base = {
+      color: '#000',
+      borderWidth: '3px',
+      borderStyle: 'solid',
+      width: '300px',
+      padding: '8px',
+    };
+  }
+
+  getFormResult() {
+    return JSON.stringify(this.form.value, null, 2);
+  }
+}
