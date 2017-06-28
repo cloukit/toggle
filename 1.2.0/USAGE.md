1ac0f51ac669bc80c2078446882cdbf7c9563600
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-06-28 23:35:28 +0200

    1.2.0

---
+++ 1.2.0/USAGE.md
@@ -0,0 +1,10 @@
+<!-- !!! will be dynamically included into cloukit.github.io component doc !!! -->
+<!-- !!! DO NOT USE UNECESSARY MARRKUP THAT BREAKS THE CORPORATE DESIGN !!! -->
+
+Use toggle inside your [Reactive Forms](https://angular.io/guide/reactive-forms).
+
+```html
+<cloukit-toggle
+  formControlName="isCatDead"
+></cloukit-toggle>
+```
---
+++ 1.2.0/src/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule, Injectable } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent } from './app.component';
+import { PreviewComponent } from './preview.component';
+import prefixAll from 'inline-style-prefixer/static';
+import { ToggleModule, CloukitToggleComponentThemeDefault } from '@cloukit/toggle';
+import { CloukitThemeService } from '@cloukit/theme';
+
+@Injectable()
+export class MyThemeService extends CloukitThemeService {
+  constructor() {
+    super();
+    const myToggleTheme = new CloukitToggleComponentThemeDefault();
+    console.log(prefixAll({ userSelect: 'none' }));
+    this.registerPrefixer((x) => {
+
+      const pre = prefixAll(x);
+      
+      return pre;
+    });
+    this.registerComponentTheme('toggle', myToggleTheme);
+  }
+};
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    CommonModule,
+    ReactiveFormsModule,
+    ToggleModule,
+  ],
+  declarations: [
+    AppComponent,
+    PreviewComponent,
+  ],
+  providers: [{ provide: CloukitThemeService, useClass: MyThemeService }],
+  bootstrap: [
+    AppComponent
+  ]
+})
+export class AppModule {}
